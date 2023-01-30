13e7d21c9586e9d18a5f8b5da4b078bc4a3b8327
anjuli kajawadekar <anjulikajawadekar@gmail.com>
2023-01-30 16:46:20 +0530

    add application

--- src/main/java/com/staffing/entity/Recruiter.java
+++ src/main/java/com/staffing/entity/Recruiter.java
@@ -16,66 +16,22 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Recruiter {
 	
 	@Id
-	@Column(name = "Recruiter_id")
+	@Column(name = "recruiter_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer Recruiter_id;
-	private String Recruiter_name;
-	private String Recruiter_email;
-	private String Role;
-	private String Password;
+	private Integer recruiter_id;
+	private String recruiter_name;
+	private String recruiter_email;
+	private String role;
+	private String password;
 	
-	@ManyToMany
-	@JoinColumn(name ="Requisition_id")
-	private Requisition requisition;
-	
-	public Recruiter(Integer recruiter_id, String recruiter_name, String recruiter_email, String role, String password,
-			Requisition requisition) {
+	public Recruiter() {
 		super();
-		Recruiter_id = recruiter_id;
-		Recruiter_name = recruiter_name;
-		Recruiter_email = recruiter_email;
-		Role = role;
-		Password = password;
-		this.requisition = requisition;
+		// TODO Auto-generated constructor stub
 	}
 	
-	public Integer getRecruiter_id() {
-		return Recruiter_id;
-	}
-	public void setRecruiter_id(Integer recruiter_id) {
-		Recruiter_id = recruiter_id;
-	}
-	public String getRecruiter_name() {
-		return Recruiter_name;
-	}
-	public void setRecruiter_name(String recruiter_name) {
-		Recruiter_name = recruiter_name;
-	}
-	public String getRecruiter_email() {
-		return Recruiter_email;
-	}
-	public void setRecruiter_email(String recruiter_email) {
-		Recruiter_email = recruiter_email;
-	}
-	public String getRole() {
-		return Role;
-	}
-	public void setRole(String role) {
-		Role = role;
-	}
-	public String getPassword() {
-		return Password;
-	}
-	public void setPassword(String password) {
-		Password = password;
-	}
-
-	public Requisition getRequisition() {
-		return requisition;
-	}
-
-	public void setRequisition(Requisition requisition) {
-		this.requisition = requisition;
-	}	
+//	@ManyToMany
+//	@JoinColumn(name ="requisition_id")
+//	private Requisition requisition;
+	
 	
 }
--- src/main/java/com/staffing/entity/Status.java
+++ src/main/java/com/staffing/entity/Status.java
@@ -1,76 +1,47 @@
 package com.staffing.entity;
 
 import java.time.LocalDate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Status {
 
 	@Id
-	@Column(name = "Recruiter_id")
+	@Column(name = "status_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer Status_id;
-	private String Status;
-	private LocalDate Status_date;
+	private Integer status_id;
+	private String status;
+	private LocalDate status_date;
 	
-	@OneToMany(mappedBy = "Candidate")
-	@JsonIgnore
-	private Integer Candidate_id;
-	
-	@OneToMany(mappedBy = "Requisition")
-	@JsonIgnore
-	private Integer Requisition_id;
-	
-	public Status(Integer status_id, String status, LocalDate status_date, Integer candidate_id,
-			Integer requisition_id) {
+	public Status() {
 		super();
-		Status_id = status_id;
-		Status = status;
-		Status_date = status_date;
-		Candidate_id = candidate_id;
-		Requisition_id = requisition_id;
+		// TODO Auto-generated constructor stub
 	}
 	
-	public Integer getStatus_id() {
-		return Status_id;
-	}
-	public void setStatus_id(Integer status_id) {
-		Status_id = status_id;
-	}
-	public String getStatus() {
-		return Status;
-	}
-	public void setStatus(String status) {
-		Status = status;
-	}
-	public Integer getCandidate_id() {
-		return Candidate_id;
-	}
-	public LocalDate getStatus_date() {
-		return Status_date;
-	}
-
-	public void setStatus_date(LocalDate status_date) {
-		Status_date = status_date;
-	}
-
-	public Integer getRequisition_id() {
-		return Requisition_id;
-	}
-
-	public void setRequisition_id(Integer requisition_id) {
-		Requisition_id = requisition_id;
-	}
-
-	public void setCandidate_id(Integer candidate_id) {
-		Candidate_id = candidate_id;
-	}	
+	
+	
+//	@OneToMany
+//	@JoinColumn(name ="candidate_id")
+//	private Candidate candidate;
+//	
+//	@OneToMany
+//	@JoinColumn(name ="candidate_id")
+//	private Requisition requisition;
+	
+//	@OneToMany(mappedBy = "Candidate")
+//	@JsonIgnore
+//	private Integer Candidate_id;
+//	
+//	@OneToMany(mappedBy = "Requisition")
+//	@JsonIgnore
+//	private Integer Requisition_id;	
+	
 }
