bfef58a3af5fc867df0e7a38307364a0225bbb48
anjuli kajawadekar <anjulikajawadekar@gmail.com>
2023-03-02 18:11:55 +0530

    StausTblRepo

--- src/main/java/com/staffing/demo/repository/StatusTblRepo.java
+++ src/main/java/com/staffing/demo/repository/StatusTblRepo.java
@@ -1,11 +1,14 @@
 package com.staffing.demo.repository;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import com.staffing.demo.entity.Requisition;
 import com.staffing.demo.entity.StatusTbl;
 
 public interface StatusTblRepo extends JpaRepository<StatusTbl, Integer> {
@@ -21,4 +24,8 @@ public interface StatusTblRepo extends JpaRepository<StatusTbl, Integer> {
 	@Query("UPDATE StatusTbl st  SET st.flag = false where requisition_id=:requisiton_id "
 			+ "and recruiter_id=:recruiter_id and candidate_id is null or candidate_id=:candidate_id")
 	void setEnabledFalse2(int recruiter_id,int requisiton_id,  int candidate_id);
+	
+	@Query("select st from StatusTbl st order by st.status_id desc")
+	@Override
+	List<StatusTbl> findAll();
 }
