2b28ef00fc61d7155a0ddc34b2a6db16c3e27e6e
soham <k12soham@gmail.com>
2023-04-12 16:02:59 +0530

    category

--- src/main/java/com/staffing/demo/entity_fd/Status_fd.java
+++ src/main/java/com/staffing/demo/entity_fd/Status_fd.java
@@ -10,27 +10,26 @@ public class Status_fd {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer Status_fd_id;
-	private String Status_fd;
+	private Integer status_fd_id;
+	private String status_fd;
 	public Status_fd() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
 	public Integer getStatus_fd_id() {
-		return Status_fd_id;
+		return status_fd_id;
 	}
-
 	public void setStatus_fd_id(Integer status_fd_id) {
-		Status_fd_id = status_fd_id;
+		this.status_fd_id = status_fd_id;
 	}
-
 	public String getStatus_fd() {
-		return Status_fd;
+		return status_fd;
 	}
 	public void setStatus_fd(String status_fd) {
-		Status_fd = status_fd;
+		this.status_fd = status_fd;
 	}
+	
+	
 
 	
 	
